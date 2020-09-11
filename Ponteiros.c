62af2d4230ff2b12a1df5ed1f9f476f06f505133
Eduardo Andrade <eduardo.a@edu.pucrs.br>
2020-09-11 06:22:38 -0300

    Update Ponteiros.c

--- Ponteiros.c
+++ Ponteiros.c
@@ -3,3 +3,27 @@
 //
 
 #include "Ponteiros.h"
+
+int indexOf(int n, int *arr, int size) {
+    int index = -1;
+    for (int i = 0; i < size; ++i) {
+        if (*(arr + i) == n) {
+            return i;
+        }
+    }
+    return index;
+}
+
+void copy(int *arrA, int *arrB, int size) {
+    for (int i = 0; i < size; ++i) {
+        *(arrB + i) = *(arrA + i);
+    }
+}
+
+void invert(int *arr, int size) {
+    for (int low = 0, high = size - 1; low < high; ++low, --high) {
+        int temp = arr[low];
+        arr[low] = arr[high];
+        arr[high] = temp;
+    }
+}
