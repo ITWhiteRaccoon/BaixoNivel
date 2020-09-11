e83d0475db57f74741fc274dc953e2ac30c145e2
Eduardo Andrade <eduardo.a@edu.pucrs.br>
2020-09-11 06:22:40 -0300

    Update Ponteiros.h

--- Ponteiros.h
+++ Ponteiros.h
@@ -5,4 +5,10 @@
 #ifndef BAIXONIVEL_PONTEIROS_H
 #define BAIXONIVEL_PONTEIROS_H
 
+int indexOf(int n, int arr[], int size);
+
+void copy(int arrA[], int arrB[], int size);
+
+void invert(int arr[], int size);
+
 #endif //BAIXONIVEL_PONTEIROS_H
